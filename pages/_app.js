0ef894489481e3a660bcd477fb629c5950fe66dd
Kisore <lakisore111@gmail.com>
2020-09-21 10:26:51 +0530

    added editor component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "semantic-ui-css/semantic.min.css";
+import Prism from "prismjs";
+import "prismjs/components/prism-markup";
+import "prismjs/components/prism-css";
+import "prismjs/themes/prism-dark.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,124 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { Fragment, useState, useEffect, useRef } from "react";
+import Editor from "react-simple-code-editor";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import theme from "prism-react-renderer/themes/dracula";
+import { Segment, Button, Dropdown } from "semantic-ui-react";
+import { exportComponentAsJPEG, exportComponentAsPNG } from "react-component-export-image";
 
-export default function Home() {
+function Home() {
+  const exampleCode = `
+(function someDemo() {
+  var test = "Hello World!";
+  console.log(test);
+})();
+
+return () => <App />;
+`;
+
+  const exportRef = useRef();
+  const [code, setCode] = useState(exampleCode);
+  const [styles, setStyles] = useState({
+    boxShadow: "4px 6px #808B96",
+    boxSizing: "border-box",
+    fontFamily: '"Dank Mono", "Fira Code", monospace',
+    fontSize: "14px",
+    ...theme.plain,
+  });
+  const [selectedTheme, setSelectedTheme] = useState("");
+
+  // useEffect(() => {
+  //   setSelectedTheme(React.lazy(() => import("prism-react-renderer/themes/oceanicNext")));
+  // }, []);
+  // console.log(selectedTheme);
+
+  const onValueChange = (code) => {
+    setCode(code);
+    // setSelectedTheme(React.lazy(() => import("prism-react-renderer/themes/dracula")));
+  };
+
+  const highlight = (code) => {
+    // const theme = React.lazy(() => import("prism-react-renderer/themes/oceanicNext"));
+    return (
+      <Highlight {...defaultProps} theme={theme} code={code} language="jsx">
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <Fragment>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </Fragment>
+        )}
+      </Highlight>
+    );
+  };
+
+  const increaseFontSize = (e, { value }) => {
+    setStyles({ ...styles, fontSize: value });
+  };
+
+  const exportCode = (e, { value }) => {
+    console.log(value);
+    // if (value === "png") exportComponentAsPNG(exportRef);
+    // else if (value === "jpg") exportComponentAsJPEG(exportRef);
+    // else return;
+  };
+
+  const sizeOptions = [
+    { text: "12px", value: "12px", key: "12px", small: "12px" },
+    { text: "14px", value: "14px", key: "14px", medium: "14px" },
+    { text: "16px", value: "16px", key: "16px", large: "16px" },
+    { text: "18px", value: "18px", key: "18px", huge: "18px" },
+  ];
+
+  const exportOptions = [
+    {
+      key: "export as jpg",
+      text: "export as jpg",
+      value: "jpg",
+    },
+    {
+      key: "export as png",
+      text: "export as png",
+      value: "png",
+    },
+  ];
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      <Segment>
+        <Dropdown
+          placeholder="select font size"
+          closeOnEscape={false}
+          selection
+          options={sizeOptions}
+          onChange={increaseFontSize}
+        />
+        <Button.Group color="teal">
+          <Button>Export</Button>
+          <Dropdown
+            className="button icon"
+            floating
+            options={exportOptions}
+            trigger={<></>}
+            onChange={exportCode}
+          />
+        </Button.Group>
+      </Segment>
+      <div ref={exportRef}>
+        <Segment padded raised style={{ marginTop: "3em", background: "#AED6F1" }}>
+          <Editor
+            value={code}
+            onValueChange={onValueChange}
+            highlight={highlight}
+            padding={10}
+            style={styles}
+          />
+        </Segment>
+      </div>
     </div>
-  )
+  );
 }
+export default Home;
