3bb2aa7050b2ef94070fe2c7d28e617536bebae7
Kisore <lakisore111@gmail.com>
2020-09-26 10:39:59 +0530

    commented particles

--- pages/index.js
+++ pages/index.js
@@ -41,16 +41,16 @@ export default function Home() {
 
   return (
     <div
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-      }}
+    // style={{
+    //   position: "absolute",
+    //   top: 0,
+    //   left: 0,
+    //   width: "100%",
+    //   height: "100%",
+    // }}
     >
       <div>
-        <Particles
+        {/* <Particles
           params={{
             particles: {
               number: {
@@ -103,7 +103,7 @@ export default function Home() {
             },
             retina_detect: false,
           }}
-        />
+        /> */}
         <div
           style={{
             position: "absolute",
